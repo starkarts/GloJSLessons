e1c75ddd9c918206f57f128c60f0051c78cd5d99
mi <miinx@yandex.ru>
2021-05-29 08:05:45 +0300

    lesson_2 ready

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 1500;
+let income = 500;
+let addExpenses = 'Интернет, такси, коммуналка';
+let deposit = true;
+let mission = 10000;
+let period = 8;
 
-alert('Hello world!');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log('Hello world!');
+console.log(addExpenses.length);
+
+console.log('Период равен '+ period + ' месяцев');
+console.log('Цель заработать '+ mission + ' долларов');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = (money + income) / 30;
+console.log(budgetDay);
